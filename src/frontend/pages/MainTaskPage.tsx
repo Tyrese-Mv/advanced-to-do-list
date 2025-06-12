7381c3d2426911d03cc010f28c98637be06c08d6
Tyrese-Mv <musawenkosi106@gmail.com>
2025-06-12 16:09:29 +0200

    Removed the create task component

--- src/frontend/pages/MainTaskPage.tsx
+++ src/frontend/pages/MainTaskPage.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Board from "./Board";
-import CreateTask from "./CreateTask";
+
 
 export default function MainTaskPage(){
     return(
         <>
-            <CreateTask/>
+            
             <Board/>
         </>
     )
