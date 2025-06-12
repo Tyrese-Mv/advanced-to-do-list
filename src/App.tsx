4650723bd202f654605db791cca9495742788635
Tyrese-Mv <musawenkosi106@gmail.com>
2025-06-12 16:04:22 +0200

    added a new route for the create task page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Route, Routes } from 'react-router-dom'
 import Login from './frontend/pages/Login'
 import Register from './frontend/pages/Register'
 import MainTaskPage from './frontend/pages/mainTaskPage'
+import CreateTask from './frontend/pages/CreateTask'
+
 function App() {
 
 
@@ -15,6 +17,7 @@ function App() {
         <Route path="/login" element= {<Login/>}/>
         <Route path="/register" element= {<Register/>}/>
         <Route path="/main" element={<MainTaskPage/>}/>
+        <Route path="/add" element={<CreateTask/>}/>
       </Routes>
     </>
   )
