7dcfc3824abd7ef660078606214ed92d94baa667
Tyrese-Mv <musawenkosi106@gmail.com>
2025-06-23 14:00:58 +0200

    Update routing in App component to redirect from home to login page
    - Replaced the Home component route with a redirect to the Login page for improved user flow.
    - Commented out the previous Home route for potential future use.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import Home from './frontend/pages/Home'
 import './App.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 import Login from './frontend/pages/Login'
 import Register from './frontend/pages/Register'
 import MainTaskPage from './frontend/pages/mainTaskPage'
@@ -13,7 +12,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element= {<Home/>}/>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        {/* <Route path="/" element= {<Home/>}/> */}
         <Route path="/login" element= {<Login/>}/>
         <Route path="/register" element= {<Register/>}/>
         <Route path="/main" element={<MainTaskPage/>}/>
